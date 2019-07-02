457160c979252af5860e8e1068c2c856a5dab9e5
hazuki <hazu.git.hazu.git@gmail.com>
2019-07-02 11:05:13 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
+  root to: 'tasks#index'
+  
+  get 'users/new'
+  get 'users/create'
     
+    #ログインフォーム
+    get 'login', to: 'sessions#new'
+    
+    #ログイン処理を担当するアクション
+    post 'login', to: 'sessions#create'
+    
+    delete 'logout', to: 'sessions#destroy'
+
+    #ユーザ登録用のフォーム
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :new, :create]
     resources :tasks
 end
