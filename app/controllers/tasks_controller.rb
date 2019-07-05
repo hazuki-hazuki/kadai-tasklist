c967106bbaec1535f3743c1e2dc06c49fc3ec2b4
hazuki <hazu.git.hazu.git@gmail.com>
2019-07-05 11:07:11 +0000

    for himo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,26 @@
 class TasksController < ApplicationController
  before_action :set_task, only: [:show, :edit, :update, :destroy]
  before_action :require_user_logged_in
+ 
 
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = Task.new(
+     content: params[:content],
+     user_id: @current_user.id
+    )
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
